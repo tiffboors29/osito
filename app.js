10d1b059d5e5d9e997efea3c7f7ba702ddd0f88d
tiffboors29 <tiffanyboorsma@gmail.com>
2015-11-08 00:04:41 -0500

    Finish site

---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+$(window).load(function(){
+  $('#menu').sticky({ topSpacing: 0 });
+  $('#showAll').addClass('active');
+});
+
+
+$('#showAll').on('click', function(){
+  $('#showRepair').removeClass('active');
+  $('#showPaint').removeClass('active');
+  $('#showAll').addClass('active');
+  $('#paint-prices').show();
+  $('#repair-prices').show();
+});
+
+$('#showRepair').on('click', function(){
+  $('#showAll').removeClass('active');
+  $('#showPaint').removeClass('active');
+  $('#showRepair').addClass('active');
+  $('#paint-prices').hide();
+  $('#repair-prices').show();
+});
+
+$('#showPaint').on('click', function(){
+  $('#showAll').removeClass('active');
+  $('#showRepair').removeClass('active');
+  $('#showPaint').addClass('active');
+  $('#paint-prices').show();
+  $('#repair-prices').hide();
+});
+
+$('#subscribe').on('click', function(){
+  if ( !$('#email').val() ){
+    $('#email').val('Please enter your email before you submit!');
+  } else {
+
+  }
+});
+
+
+$('.myform').submit(function (e) {
+  var $this = $(this);
+  $.ajax({
+      type: $this.attr("method"),
+      url: $this.attr("action"),
+      data: $this.serialize(),
+      dataType: "json",
+      contentType: "application/json"
+    })
+    .done(function (data) {
+      console.log("Response data:", data);
+    });
+});
+
+
